9bf910662610da114cc2b729900c4b7f37e30593
Fabianhenrick <fabianhenrick@gmail.com>
2024-06-25 22:37:17 -0300

    post with express

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ import express from "express";
  */
 
 const app = express();
+app.use(express.json());
+
+const users = [];
+app.post("/usuarios", (req, res) => {
+  console.log(req.body);
+  res.send("ok, aqui deu certo!");
+});
 
 app.get("/usuarios", (req, res) => {
   res.send("Ok, deu bom!");
